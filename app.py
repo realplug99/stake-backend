6dac44fce9bab0aaca3991c7ebaccee10665c1a7
realplug99 <lngouanolinga@gmail.com>
2026-04-16 18:56:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
-import os
-import sys
+from flask import Flask
 
+app = Flask(__name__)
 
-sys.path.insert(0, os.path.dirname(__file__))
-
-
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python v' + sys.version.split()[0] + '\n'
-    response = '\n'.join([message, version])
-    return [response.encode()]
+@app.route("/")
+def home():
+    return "✅ Server is live"
