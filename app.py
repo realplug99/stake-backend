3da4a9791b39b285b2b00f19f8583f3637b4def4
realplug99 <lngouanolinga@gmail.com>
2026-04-16 19:07:29 -0400

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask_cors import CORS
 from flask import Flask
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
     return "✅ Server is live"
+    @app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
